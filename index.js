52e0f26f5ff959c9ecd6fa8b952f7d54f4cbde13
Klaus Kientzle <klaus.kientzle@gammapartners.com>
2019-03-26 18:08:41 -0700

    zeit now func added

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    mongoose = require('mongoose'),
+    config = require('./config/database');
+
+mongoose.connect(config.database);
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+})
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+const users = require('./routes/users');
+const products = require('./routes/products');
+const sessions = require('./routes/sessions');
+
+app.use('/users', users.unprotected)
+app.use('/products', products.unprotected)
+app.use('/sessions', sessions.unprotected)
+
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+})
+
+app.listen(4000, () => { console.log('Express Server on 4000') })
+
+//module.exports = app
