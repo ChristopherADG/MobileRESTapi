b40c2b1fd192dad49b96d1967d96c27eff0ccc02
Klaus Kientzle <klaus.kientzle@gammapartners.com>
2019-05-02 20:28:52 -0600

    Get product in cart implementation is on dev.

--- routes/cart.js
+++ routes/cart.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const Cart = require('../models/Cart');
-
+const Product = require('../models/Product');
 
 //All items in cart
 router.route('/').get((req, res) => {
@@ -15,15 +15,38 @@ router.route('/').get((req, res) => {
     })
 });
 
-// Get cart for user
-router.route('/:email').get((req, res) => {
-    Cart.findById(req.params.email, (err, cart) => {
+function getProductsFromCart(cart) {
+    console.log(cart)
+    var arr = []
+    cart.forEach(function(entry) {
+        console.log(entry["user_email"]);
+        Product.findById(entry["item_id"], (err, product) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(product["name"])
+                arr.push(product)
+            }
+        });
+        console.log(arr)
+    });
+    return arr
+}
+
+// Get cart.item for user
+router.route('/find/:email').get((req, res) => {
+
+    var productsInCart = []
+
+    Cart.find({user_email: req.params.email}, (err, cart) => {
         if (err) {
             console.log(err);
         } else {
-            res.json(cart);
+            productsInCart = getProductsFromCart(cart)
+            console.log(productsInCart)
+            res.json(productsInCart)
         }
-    })
+    });
 });
 
 //Add Product
